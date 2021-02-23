b487ab25608ca256ad51bc61599cdc1bd00f8287
ohidul alam <mdtasib442@gmali.com>
2021-02-23 16:53:54 +0600

    pracite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name='Tasib' profession='Programmar'></Person>
+        <Person name='Rafi' profession='Developer'></Person>
+        <Person name='Rakib' profession='Student'></Person>
       </header>
     </div>
   );
+}
+function Person(props) {
+  const personStyle = {
+    border: '2px solid red',
+    margin: '20px',
+    padding: '20px'
+  }
+  return (
+    <div style={personStyle}>
+      <h3>My Name Is {props.name}</h3>
+      <p>I am a {props.profession}</p>
+    </div>
+  )
 }
 
 export default App;
