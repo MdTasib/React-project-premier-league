0bf8b7e6944a5d6fceefc78aac7819dedc25e12e
ohidul alam <mdtasib442@gmali.com>
2021-02-23 22:52:22 +0600

    praciice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const products = [
@@ -17,10 +17,30 @@ function App() {
         } */}
 
         <Counter></Counter>
+        <Users></Users>
 
       </header>
     </div>
   );
+}
+
+function Users() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+  return (
+    <div>
+      <h2>Dynamic load data : {users.length}</h2>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Counter() {
