2ac72babdef9fbf5af3087d36c0d09a5dea3e79a
ohidul alam <mdtasib442@gmali.com>
2021-02-24 17:55:30 +0600

    d

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Count from './Components/UseState'
+import Person from './Components/Person'
 
 function App() {
-  const products = [
-    { name: 'Iphone', price: '$222' },
-    { name: 'Vivo', price: '$123' },
-    { name: 'Nokia', price: '$52' },
-    { name: 'Nokia', price: '$52' },
-  ]
-
   return (
     <div className="App">
-      <header className="App-header">
-        {/* {
-          products.map(pd => <Product product={pd}></Product>)
-        } */}
-
-        <Counter></Counter>
-        <Users></Users>
-        <Count />
-
-      </header>
-    </div>
-  );
-}
-
-function Users() {
-  const [users, setUsers] = useState([])
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setUsers(data));
-  }, [])
-  return (
-    <div>
-      <h2>Dynamic load data : {users.length}</h2>
-      <ul>
-        {
-          users.map(user => <li>{user.name}</li>)
-        }
-      </ul>
-    </div>
-  )
-}
-
-function Counter() {
-  const [count, setCount] = useState(0)
-  return (<div>
-    <h1>Counter: {count}</h1>
-    <button onClick={() => { setCount(count + 1) }}>+</button>
-    <button onClick={() => { setCount(count - 1) }}>-</button>
-  </div>
-  )
-}
-
-function Product(props) {
-
-  const productStyle = {
-    backgroundColor: 'gray',
-    color: '#fff',
-    width: '200px',
-    margin: '15px',
-    padding: '15px'
-  }
-
-  const { name, price } = props.product;
-  return (
-    <div style={productStyle}>
-      <h3>{name}</h3>
-      <h2>{price}</h2>
-      <button>Buy Now</button>
+      <Person />
     </div>
   )
 }
---
+++ src/Components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Person(props) {
+    return (
+        <div>
+            <h2>Your name is :</h2>
+            <h3>Your age is : </h3>
+        </div>
+    )
+}
